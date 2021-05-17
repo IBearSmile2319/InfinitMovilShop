3d59ae20f9e9286bf940c53ad0d21401c37cfee0
IBearSmile2319 <78711486+IBearSmile2319@users.noreply.github.com>
2021-05-16 19:05:07 -0500

    refactorizando

--- api/routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-const {Router}=require('express'),
-router=Router()
-const { SignupController, activateController } = require('../controllers/AuthController')
-
-router.post('/signup',SignupController)
-router.post('/activation',activateController)
-
-module.exports=router
--- client/src/Pages/404/Error.jsx
+++
@@ -1,9 +0,0 @@
-const Error = () => {
-    return (
-        <div>
-            error 404
-        </div>
-    )
-}
-
-export default Error
